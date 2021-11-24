a297e4c092599132a3a75a5d8fc6f1b1ebd116c3
litianhua <18702971504@163.com>
2021-11-24 14:02:03 +0800

    引入jwt验证

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { UserModule } from './logical/user/user.module'
+import { AuthService } from './logical/auth/auth.service'
+import { AuthModule } from './logical/auth/auth.module'
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, AuthModule],
   controllers: [AppController],
   providers: [AppService]
 })
