345cd660dff432a5afef1e34b5d2a6a76fb3512b
litianhua <18702971504@163.com>
2021-11-24 19:45:48 +0800

    捕获Http异常

---
+++ src/filter/http-exception.filter.ts
@@ -0,0 +1,31 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException
+} from '@nestjs/common'
+import { Response, Request } from 'express'
+import { Logger } from 'src/utils/log4js'
+
+@Catch()
+export class HttpExceptionFilter implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp()
+    const response = ctx.getResponse<Response>()
+    const request = ctx.getRequest<Request>()
+    const status = exception.getStatus()
+    const logFormat = ` <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+    Request original url: ${request.originalUrl}
+    Method: ${request.method}
+    IP: ${request.ip}
+    Status code: ${status}
+    Response: ${exception.toString()} \n  <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+    `
+    Logger.info(logFormat)
+    response.status(status).json({
+      statusCode: status,
+      error: exception.message,
+      msg: 'fail'
+    })
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import * as express from 'express'
 import { AppModule } from './app.module'
 import { TransformInterceptor } from './intercepter/transform.interceptor'
 import { LoggerMiddleware } from './middleware/logger.middleware'
+import { HttpExceptionFilter } from './filter/http-exception.filter'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
@@ -10,6 +11,8 @@ async function bootstrap() {
   app.use(express.urlencoded({ extended: true }))
   app.use(LoggerMiddleware)
   app.useGlobalInterceptors(new TransformInterceptor())
+  // 过滤处理HTTP异常
+  app.useGlobalFilters(new HttpExceptionFilter())
   await app.listen(3000)
 }
 bootstrap()
