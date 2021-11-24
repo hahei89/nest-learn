bfbfdabb36368cacc4cd3a037623db5b2614a90f
litianhua <18702971504@163.com>
2021-11-24 17:34:11 +0800

    添加日志

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core'
+import * as express from 'express'
 import { AppModule } from './app.module'
+import { TransformInterceptor } from './intercepter/transform.interceptor'
+import { LoggerMiddleware } from './middleware/logger.middleware'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  app.use(express.json()) // For parsing application/json
+  app.use(express.urlencoded({ extended: true }))
+  app.use(LoggerMiddleware)
+  app.useGlobalInterceptors(new TransformInterceptor())
   await app.listen(3000)
 }
 bootstrap()
