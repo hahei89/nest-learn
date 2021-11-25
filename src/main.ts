202a9acdd23a47acf9b1a02676cc861ff641609f
litianhua <18702971504@163.com>
2021-11-25 16:44:11 +0800

    加入swagger生成文档

--- src/main.ts
+++ src/main.ts
@@ -5,16 +5,26 @@ import { TransformInterceptor } from './intercepter/transform.interceptor'
 import { LoggerMiddleware } from './middleware/logger.middleware'
 import { AnyExceptionFilter } from './filter/any-exception.filter'
 import { HttpExceptionFilter } from './filter/http-exception.filter'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.use(express.json()) // For parsing application/json
   app.use(express.urlencoded({ extended: true }))
-  app.use(LoggerMiddleware)
+  app.use(new LoggerMiddleware().use)
   app.useGlobalInterceptors(new TransformInterceptor())
   // 过滤处理HTTP异常
   app.useGlobalFilters(new HttpExceptionFilter())
   app.useGlobalFilters(new AnyExceptionFilter())
+  // 配置swagger
+  const options = new DocumentBuilder()
+    .setTitle('Nest zero to one')
+    .setDescription('The nest-zero-to-one API desciption')
+    .setVersion('1.0')
+    .addTag('test')
+    .build()
+  const document = SwaggerModule.createDocument(app, options)
+  SwaggerModule.setup('api-doc', app, document)
   await app.listen(3000)
 }
 bootstrap()
