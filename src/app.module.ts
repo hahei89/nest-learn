bf3200a3ee564afc46f10f36e55ea42fc7584ed8
litianhua <18702971504@163.com>
2021-11-25 16:11:49 +0800

    角色权限控制

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,10 @@ import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { UserModule } from './logical/user/user.module'
 import { AuthModule } from './logical/auth/auth.module'
-import { UserController } from './logical/user/user.controller'
+import { CommodityModule } from './logical/commodity/commodity.module'
 
 @Module({
-  imports: [UserModule, AuthModule],
+  imports: [UserModule, AuthModule, CommodityModule],
   controllers: [AppController],
   providers: [AppService]
 })
